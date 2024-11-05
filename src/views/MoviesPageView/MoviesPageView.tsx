15a0c30122a69b0196badcb65258523e10b9f2cd
Denys Strashniuk <d.strashniuk@gmail.com>
2024-11-05 16:02:29 +0100

    add changeQuery for MoviesPageView

--- src/views/MoviesPageView/MoviesPageView.tsx
+++ src/views/MoviesPageView/MoviesPageView.tsx
@@ -1,3 +1,19 @@
+import { useState } from 'react';
+import Section from '../../components/Section/Section';
+
 export default function MoviesPageView() {
-  return <h1>MoviesPageView</h1>;
+  const [movies, setMovies] = useState([]);
+  const [query, setQuery] = useState('');
+
+  const changeQuery = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const q = e.currentTarget.value;
+
+    setQuery(q);
+  };
+
+  return (
+    <Section title="Find a movie">
+      <input type="text" onChange={changeQuery} value={query} />
+    </Section>
+  );
 }
