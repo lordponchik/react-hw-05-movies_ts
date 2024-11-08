0c4e5d860ba1d7847d8550728a1a9c2708482261
Denys Strashniuk <d.strashniuk@gmail.com>
2024-11-08 14:33:22 +0100

    del Header in App

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Header from './Header/Header';
 import HomePageView from '../views/HomePageView/HomePageView';
 import MoviesPageView from '../views/MoviesPageView/MoviesPageView';
 import MovieDetailsPageView from '../views/MovieDetailsPageView/MovieDetailsPageView';
