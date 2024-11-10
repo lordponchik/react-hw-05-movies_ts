9791978c4d0840b8f53b6b8556e6065d08ee9d55
Denys Strashniuk <d.strashniuk@gmail.com>
2024-11-10 11:39:47 +0100

    add ReviewsView and styles

--- src/views/ReviewsView/ReviewsView.tsx
+++ src/views/ReviewsView/ReviewsView.tsx
@@ -1,3 +1,44 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { fetchMoviesReviews } from '../../services/api';
+import s from './ReviewsView.module.css';
+
+interface IReview {
+  id: string;
+  author: string;
+  content: string;
+}
+
 export default function ReviewsView() {
-  return <h1>ReviewsView</h1>;
+  const [reviews, setReviews] = useState<IReview[] | null>(null);
+  const { movieId } = useParams();
+
+  useEffect(() => {
+    async function requestMovieReviews() {
+      const { results } = await fetchMoviesReviews(Number(movieId));
+
+      setReviews(results);
+    }
+
+    requestMovieReviews();
+  }, [movieId]);
+
+  return (
+    <>
+      {reviews && (
+        <ul className={s.reviews}>
+          {reviews.map(({ id, author, content }) => {
+            return (
+              <li key={id} className={s.reviewsItem}>
+                <h3 className={s.title}>
+                  <span className={s.titleName}>Author: </span> {author}
+                </h3>
+                <p className="content">{content}</p>
+              </li>
+            );
+          })}
+        </ul>
+      )}
+    </>
+  );
 }
