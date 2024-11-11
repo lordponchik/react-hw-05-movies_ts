c23dbd451a0dd5a0576925c834556aa8e081ced4
Denys Strashniuk <d.strashniuk@gmail.com>
2024-11-11 13:04:24 +0100

    change useState to useQuery for HomePageView

--- src/views/HomePageView/HomePageView.tsx
+++ src/views/HomePageView/HomePageView.tsx
@@ -1,35 +1,28 @@
-import { useEffect, useState } from 'react';
 import { fetchTrendingMovies } from '../../services/api';
 import Section from '../../components/Section/Section';
 import MoviesList from '../../components/MoviesList/MoviesList';
 import InformationMessage from '../../components/InformationMessage/InformationMessage';
 import Loader from '../../components/Loader/Loader';
+import { useQuery } from '@tanstack/react-query';
+
+async function requestMovies() {
+  const { results } = await fetchTrendingMovies();
+  return results;
+}
 
 export default function HomePageView() {
-  const [movies, setMovies] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    async function requestMovies() {
-      setIsLoading(true);
-
-      try {
-        const { results } = await fetchTrendingMovies();
-        setMovies(results);
-      } catch (error) {
-      } finally {
-        setIsLoading(false);
-      }
-    }
-
-    requestMovies();
-  }, []);
+  const { data, isPending, isError } = useQuery({
+    queryKey: ['trendingsMovies'],
+    queryFn: requestMovies,
+    refetchOnWindowFocus: false,
+  });
 
   return (
     <>
       <Section title="Trending today">
-        {isLoading && <Loader />}
-        {movies ? <MoviesList movies={movies}></MoviesList> : <InformationMessage />}
+        {isPending && <Loader />}
+        {(isError || data?.length === 0) && <InformationMessage />}
+        {data && <MoviesList movies={data}></MoviesList>}
       </Section>
     </>
   );
