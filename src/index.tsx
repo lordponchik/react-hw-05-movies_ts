0bb5cd5595187db76e0aa4a9fcbd2d70be4efd55
Denys Strashniuk <d.strashniuk@gmail.com>
2024-11-11 13:03:59 +0100

    add queryClient for index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
 import { BrowserRouter } from 'react-router-dom';
 
+const queryClient = new QueryClient();
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   // <React.StrictMode>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </QueryClientProvider>
+
   // </React.StrictMode>
 );
