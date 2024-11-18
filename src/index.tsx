dca0f182245c0d4b2b8ee8c40e50bc86ae7d9c31
Denys <d.strashniuk@gmail.com>
2024-11-18 10:00:58 +0100

    add basename for BrowserRouter in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,18 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './components/App';
-import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./components/App";
+import { BrowserRouter } from "react-router-dom";
 
 const queryClient = new QueryClient();
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 root.render(
   // <React.StrictMode>
   <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
+    <BrowserRouter basename="/react-hw-05-movies_ts">
       <App />
     </BrowserRouter>
   </QueryClientProvider>
