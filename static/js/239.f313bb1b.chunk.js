a1e9184805e5b264cceb6cd7a59b538ee4b267b8
Denys <strashnyuk.denis@gmail.com>
2024-11-19 13:53:03 +0100

    Updates

---
+++ static/js/239.f313bb1b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_hw_05_movies_ts=self.webpackChunkreact_hw_05_movies_ts||[]).push([[239],{239:(e,s,t)=>{t.r(s),t.d(s,{default:()=>n});var i=t(475);const r={section:"ErrorView_section__V-eMo",title:"ErrorView_title__ouR0E",link:"ErrorView_link__mTG8Z"};var c=t(579);function n(){return(0,c.jsxs)("section",{className:r.section,children:[(0,c.jsx)("h1",{className:r.title,children:"404 - Page not found"}),(0,c.jsx)(i.N_,{to:"/",className:r.link,children:"Back to Homepage"})]})}}}]);
+//# sourceMappingURL=239.f313bb1b.chunk.js.map
